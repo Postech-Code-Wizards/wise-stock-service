70c3d714e694cb893f74b77335d3c389a04c68f7
Vitor Hugo de Souza <vito.hs@hotmail.com>
2025-07-14 23:21:42 -0300

    adicionado readme e alterado para aceitar listagem em baixa e reposição de estoque

--- src/main/java/br/com/wise/stock_service/infrastructure/configuration/RabbitMQConfig.java
+++ src/main/java/br/com/wise/stock_service/infrastructure/configuration/RabbitMQConfig.java
@@ -1,5 +1,7 @@
 package br.com.wise.stock_service.infrastructure.configuration;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
@@ -14,10 +16,22 @@ public class RabbitMQConfig {
     public static final String EXCHANGE_NAME = "stock-service";
     public static final String STOCK_REPOR_QUEUE = "stock-repor-queue";
     public static final String STOCK_BAIXA_QUEUE = "stock-baixa-queue";
+    public static final String STOCK_RESPOSTA_QUEUE = "stock-resposta-queue";
+    public static final String STOCK_RESPOSTA_ROUNTING_KEY = "stock-resposta";
 
     @Bean
     public DirectExchange exchange() {
         return new DirectExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Queue respostaQueue() {
+        return new Queue(STOCK_RESPOSTA_QUEUE, true);
+    }
+
+    @Bean
+    public Binding stockBinding(Queue respostaQueue, DirectExchange exchange) {
+        return BindingBuilder.bind(respostaQueue).to(exchange).with(STOCK_RESPOSTA_ROUNTING_KEY);
     }
 
     @Bean
